c6c8ad920731a764a1892728cdd959c434f008aa
Fynn Leitow <fynn.leitow@perfood.de>
2019-10-20 22:37:31 +0300

    changed to 70x70 bounding box and type csv

--- elias_mail.py
+++ elias_mail.py
@@ -14,25 +14,32 @@ def get_stuff(dataset):
     print('GET')
     return dataset.getInfo()
 
+
 def chunk_iter(it, size):
     while True:
         chunk = itertools.islice(it, size)
-        if not chunk: return
+        if not chunk:
+            return
         yield chunk
+
 
 # TODO: break into smaller chunks on failure to retrieve
 def fetch_points(df, start='2015-01-01', end='2015-12-31'):
-    lon, lat = df.location_x, df.location_y
+    # lon, lat = df.location_x, df.location_y
+    lon, lat = df.longitude, df.latitude
 
     for iis in chunk_iter(iter(range(len(df))), 10):
         iis=list(iis)
 
-        print(iis[-1])
-        if os.path.exists(f'{iis[-1]}.json'): continue
+        if len(iis) > 0:
+            print(iis[-1])
+            if os.path.exists(f'{iis[-1]}.csv'):
+                continue
+        else:
+            continue
 
-        GEOM = ee.Geometry.MultiPoint(
-            [ [lon[i], lat[i]] for i in iis ]
-        ).buffer(60)
+        boxes = [ee.Geometry.Point([lon[i], lat[i]]).buffer(35).bounds() for i in iis]
+        GEOM = ee.Geometry.MultiPolygon(boxes)
 
         dataset = (
             ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
@@ -41,24 +48,22 @@ def fetch_points(df, start='2015-01-01', end='2015-12-31'):
             .getRegion(GEOM, 30)
         )
 
-        print(iis[-1])
+        print('current chunk until index:', iis[-1])
         try:
             e = get_stuff(dataset)
             print(iis[-1], 'evaluated')
-            with open(f'{iis[-1]}.json', 'wt') as fh:
-                json.dump(e, fh)
+            df = pd.DataFrame(e)
+            df.to_csv(f'{iis[-1]}.csv')
         except Exception as ex:
             print('Couldnt retrieve', ex)
 
-try:
-	os.chdir('/home/dario/_dsp/code')
-except:
-	pass
 
 ee.Initialize()
 
-df = pd.read_csv("/home/dario/_dsp/data/aam6527_Bastin_Database-S1.csv", sep=";")
+# df = pd.read_csv("/home/dario/_dsp/data/aam6527_Bastin_Database-S1.csv", sep=";")
+df = pd.read_csv("data/bastin_db_cleaned.csv", sep=",")
 df = df.loc[df["dryland_assessment_region"] == 'Australia']
+# fetch_points(df.iloc[:20,:])
 fetch_points(df)
 
 
