ab75b90fbf90e9da292cd688e735b0fd1ee69538
Dario Hett <40806448+DarioHett@users.noreply.github.com>
2019-11-01 19:17:25 +0200

    building pipeline v0.1

--- elias_mail.py
+++ elias_mail.py
@@ -23,11 +23,11 @@ def chunk_iter(it, size):
 
 
 # TODO: break into smaller chunks on failure to retrieve
-def fetch_points(df, start='2015-01-01', end='2015-12-31'):
-    lon, lat = df.location_x, df.location_y
-    # lon, lat = df.longitude, df.latitude
+def fetch_points(df, start='2015-01-01', end='2015-12-31', collection="LANDSAT/LC08/C01/T1_SR"):
+    #lon, lat = df.location_x, df.location_y
+    lon, lat = df.longitude, df.latitude
 
-    for iis in chunk_iter(iter(range(len(df))), 10):
+    for iis in chunk_iter(iter(range(len(df))), 2):
         iis=list(iis)
 
         if len(iis) > 0:
@@ -42,7 +42,8 @@ def fetch_points(df, start='2015-01-01', end='2015-12-31'):
 
         dataset = (
             #ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
-             ee.ImageCollection("MODIS/006/MCD64A1") 
+            #ee.ImageCollection("MODIS/006/MCD64A1") 
+             ee.ImageCollection(collection) 
             .filterDate(start, end)
             .filterBounds(GEOM)
             .getRegion(GEOM, 30)
@@ -53,17 +54,17 @@ def fetch_points(df, start='2015-01-01', end='2015-12-31'):
             e = get_stuff(dataset)
             print(iis[-1], 'evaluated')
             df = pd.DataFrame(e)
-            df.to_csv(f'tree-cover/data/{iis[-1]}_{start}_{end}.csv')
+            df.to_csv(f'data/{iis[-1]}_{start}_{end}.csv')
         except Exception as ex:
             print('Couldnt retrieve', ex)
 
 
-ee.Initialize()
+#ee.Initialize()
 
-df = pd.read_csv("tree-cover/data/aam6527_Bastin_Database-S1.csv", sep=";")
+#df = pd.read_csv("data/aam6527_Bastin_Database-S1.csv", sep=";")
 #df = pd.read_csv("data/bastin_db_cleaned.csv", sep=",")
-df = df.loc[df["dryland_assessment_region"] == 'Australia']
+#df = df.loc[df["dryland_assessment_region"] == 'Australia']
 #fetch_points(df.iloc[:20,:])
-fetch_points(df)
+#fetch_points(df)
 
 
---
+++ fetch_features.py
@@ -0,0 +1,73 @@
+""" Takes an arriving input.csv file and first checks if the respective 
+    locations have already been fetched and reside in the db.csv.
+    Otherwise proceed to fetch the points which are not yet in db.csv and
+    add them to db.csv.
+    Currently just fetches landsat data, more to come.
+    Todo:
+        - Generalize fetching to add more data sets and specific columns.
+        - Fix the fetching function from elias
+            -- Currently does not finish after fetching all the points.
+                Probably some inifinite loop.
+        - Improve performance overall
+            -- currently raping pandas beauty 
+                b/c I used R the last 2 semesters X_X
+        
+"""
+import ee
+import os
+import pandas as pd
+import backoff
+import itertools
+#import elias_mail
+
+def not_in_db(df, db):
+    """ Takes the target data frame and the existing database object.
+        Returns the splitted dataframe for existing and non-existant entries.
+    """
+    df_ex = df[~(df[['longitude', 'latitude']]
+            .isin(db[['longitude', 'latitude']]))
+        .any(axis=1)]
+    df_in = df[(df[['longitude', 'latitude']]
+            .isin(db[['longitude', 'latitude']]))
+        .any(axis=1)]
+    df_ex = df_ex.reset_index()
+    df_in = df_ex.reset_index()
+    return df_ex, df_in
+
+def extend_df_landsat(df, chunk_size = 2):
+    """ Needs generalization to incorporate multiple data sets.
+        Currently adds landsat data.
+    """
+    cols = ['B1','B2','B3','B4','B5','B6','B7','B10','B11','sr_aerosol',
+            'pixel_qa', 'radsat_qa']
+    i = chunk_size-1 
+    for _ in cols:
+        df[_] = None
+    while i < len(df):
+        gee_data = pd.read_csv(f"data/{i}_2015-01-01_2015-12-31.csv", sep=",", header=1)
+        print(f"File: {i}_2015-01-01_2015-12-31.csv")
+        #print(gee_data[cols])
+        for j in range(i-chunk_size-1,i+1):
+            dist = ((gee_data.longitude - df.longitude[j])**2 + (gee_data.latitude - df.latitude[j])**2 )
+            values = gee_data.loc[dist[dist == dist.min()].index,cols].median()
+            for k in range(len(cols)):
+                df[cols[k]].iloc[j] = values[k]
+            print(values)
+
+        i += min(chunk_size, max(len(df)-1-i,1))
+    return df
+
+def main():
+    os.chdir('/home/dario/_py/tree-cover')
+    ee.Initialize()
+    df = pd.read_csv("data/input.csv", sep=",")
+    db = pd.read_csv("data/db.csv", sep=",")
+    df_ex, df_in = not_in_db(df, db)
+    # elias_mail.fetch_points(df_ex) #Does not finish?
+    df_ex = extend_df_landsat(df_ex)
+    db.append(df_ex)
+    # Lets not overwrite our dummy data just yet.
+    # db.to_csv('data/db.csv', sep=',') 
+
+main()
+
