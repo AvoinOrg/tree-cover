5ac86898515417bd056bf3b08bd17196f7757078
Fynn Leitow <fynn.leitow@perfood.de>
2019-11-04 20:03:49 +0200

    Local changes, shapefile export, tried planet dataset but nothing for our points of interest.

--- data/info.md
+++ data/info.md
@@ -8,6 +8,17 @@ The CSV files in this directory are ignored by git, but should contain the files
 ```bash
 cd dataxsv partition --filename region_{}.csv dryland_assessment_region by_region bastin_db_cleaned.csv
 cd by_region
-# did that, but they are not used currently...
+
+#did that, but they are not used currently...
 ls | xargs -I data /bin/zsh -c 'xsv split -s 5300 --filename "data_{}.csv" splits data'
 ```
+
+### Legend
+
+| File                | Description        |
+| ------------------- | ------------------ |
+| `Australia2015.csv` | Luca's full export |
+|                     |                    |
+|                     |                    |
+|                     |                    |
+|                     |                    |
--- prepare/prepare_for_GEE.py
+++ prepare/prepare_for_GEE.py
@@ -49,18 +49,20 @@ def generate_subset(data, index=0, region='Australia', n=1000, fmt='csv'):
     end = data.shape[0] if data.shape[0] < (index+1)*n else (index+1)*n
     sub_df = data[data['dryland_assessment_region'] == region].iloc[index:end,:]
     # copy = sub_df.drop(['longitude', 'latitude'], axis=1)
-    if fmt == csv:
+    if fmt == 'csv':
         sub_df.to_csv(f'{region}_{index*n}_to_{end}.csv', header=True, index=None)
     elif fmt == 'shp':
-        sub_df['geometry'] = sub_df.apply(lambda: x: Point(float(x.longitude), float(x.latitude)), axis=1)
-        sub_df = gpd.GeoDataFrame(sub_df, geometry='geometry')
+        sub_df['geometry'] = sub_df.apply(lambda x: Point(float(x.longitude), float(x.latitude)), axis=1)
+        sub_df = gpd.GeoDataFrame(sub_df, geometry='geometry', crs="+init=epsg:4326")
         sub_df.to_file(f'{region}_{index*n}_to_{end}.shp', driver='ESRI Shapefile')
+    else:
+        print(f"Don't know {fmt}.")
         
     
 # Convert the long/lat - points into a 70 * 70 area around it for region selection
 data = pd.read_csv(clean_file_location)
 #add_rectangle_points(data)
 
-sub = generate_subset(data, index=0, n=500, fmt='csv') # change params here
+generate_subset(data, index=0, n=500, fmt='shp') # change params here
 
 
