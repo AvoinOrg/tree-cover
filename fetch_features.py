bf176275420605d25e7e8dea6a1e279a5fd001f4
Dario Hett <40806448+DarioHett@users.noreply.github.com>
2019-11-05 20:41:26 +0200

    single_point_populater() added.

--- fetch_features.py
+++ fetch_features.py
@@ -57,6 +57,65 @@ def extend_df_landsat(df, chunk_size = 2):
         i += min(chunk_size, max(len(df)-1-i,1))
     return df
 
+def single_point_populater(df, prep_method, aggregate_method,verbose=False):
+    """ Takes single points of an arriving dataframe for a given fetching target.
+        Prepares the data for a given method of preparation.
+        Fetches the data point by point accordingly.
+        Aggregates the single point data and adds it to the dataframe.
+        Returns the populated dataframe.
+    """
+    if verbose: print('Populating data.')
+    df, info = prep_method(df, verbose=verbose)
+    points = zip(df.longitude, df.latitude)
+    for lon, lat in points:
+        fetched_data = single_fetch(lon, lat, collection=info['collection'],verbose=verbose)
+        values = aggregate_method(fetched_data, lon, lat, info['cols'],verbose=verbose)
+        df.loc[(df['latitude']==lat) & (df['longitude']==lon),
+               info['cols']] = [values]
+    return df
+
+def landsat_aggregate_method(fetched_data, lon, lat, columns,verbose=False):
+    if verbose: print('Aggregating data')
+    dist = ((fetched_data.longitude - lon)**2 + 
+            (fetched_data.latitude - lat)**2)
+    values = fetched_data.loc[dist[dist == dist.min()].index, columns].median()
+    return values
+
+def landsat_prepare_method(df, verbose=False):
+    """ Takes any dataframe, gives it the respective columns 
+        with default None values and returns the requested columns
+        of landsat data. Also overrides old columns.
+        Gives a fetching function the required information (columns)
+        and prepares the dataframe into a clean format where the 
+        columns will a) exist and b) be None.
+    """
+    if verbose: print('Preping data')
+    info = {'collection': 'LANDSAT/LC08/C01/T1_SR',
+            'cols': ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B10', 'B11', 'sr_aerosol',
+                    'pixel_qa', 'radsat_qa'],
+            }
+    for _ in info['cols']:
+        df[_] = None
+    return df, info
+
+def single_fetch(lon, lat, start='2015-01-01', end='2015-12-31',
+                 collection='LANDSAT/LC08/C01/T1_SR', verbose=False):
+    """ Fetches data for a single point given by lon(gitude) and lat(itude).
+        Returns a pandas dataframe.
+    """
+    if verbose: print('fetching data')
+    pt = ee.Geometry.Point([lon, lat]).buffer(35).bounds()
+    GEOM = ee.Geometry.MultiPolygon([pt])
+
+    dataset = (ee.ImageCollection(collection)
+        .filterDate(start, end)
+        .filterBounds(GEOM)
+        .getRegion(GEOM, 30)
+    ).getInfo()
+    dataset = pd.DataFrame(dataset[1:],columns=dataset[0])
+    return dataset
+
+
 def main():
     os.chdir('/home/dario/_py/tree-cover')
     ee.Initialize()
@@ -64,7 +123,12 @@ def main():
     db = pd.read_csv("data/db.csv", sep=",")
     df_ex, df_in = not_in_db(df, db)
     # elias_mail.fetch_points(df_ex) #Does not finish?
-    df_ex = extend_df_landsat(df_ex)
+    artificial_data = {'index':1337, 'longitude':13.37, 'latitude':13.37}
+    df_ex = df_ex.append(artificial_data, ignore_index=True)
+    if len(df_ex) & len(df_ex) < 100:
+        df_ex = single_point_populater(df_ex, landsat_prepare_method,
+                                       landsat_aggregate_method,
+                                       verbose=True)
     db.append(df_ex)
     # Lets not overwrite our dummy data just yet.
     # db.to_csv('data/db.csv', sep=',') 
