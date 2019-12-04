197621b0004d14bba3ed852061632dec0e88b1b7
Fynn Leitow <19202953+LyteFM@users.noreply.github.com>
2019-12-04 17:02:30 +0200

    Faster training.

--- train.py
+++ train.py
@@ -8,34 +8,45 @@
 """
 from sklearn import ensemble as en
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 from sklearn.svm import SVR
+import lightgbm as lgb
 import numpy as np
 import pandas as pd
 import os
 import joblib as jl
 import matplotlib.pyplot as plt
-from utils import timer
+from utils import timer, dict_from_kwargs
+
+#plt.rcParams["figure.figsize"] = (30,12)
+#plt.rcParams["font.size"] = 20
 
 # global params as I'm too lazy to build a CLI
-path = 'data/features_three_months_full.parquet' # 'data/vegetation_index_features_aggregated.parquet' #  'data/features_three_months_improved.parquet' #  "data/df_empty_dummy.csv" # 
+path = 'data/features_three_months_full.parquet' # 'data/features_three_months_improved.parquet' # 'data/vegetation_index_features_aggregated_all.parquet' # 'data/vegetation_index_features_aggregated.parquet' # "data/df_empty_dummy.csv" #   
 np.random.seed(42)
 w_dir = '.' # '/home/dario/_py/tree-cover' # 
-model_name = "model_sentinel_transform_stratify_huber.joblib" # "model_landsat_median_sds.joblib" # 
+model_name = "model_sentinel_logtrans_stratified_mae_3months_dart_depth8_1000.joblib" #  "model_sentinel_logtrans_stratified_mae_allveg_lgbm_depth12_2000.joblib" # 
 
 
 do_train = True
 do_transform = True # logarithmic transform of y
-do_scale_X = False # use a MinMaxScaler to bring the data into a range bewteen -1 and 1
-do_weight = False # assign a weight to each feature s.t. those occuring less frequent will have higher weights
 do_stratify = True # only take an approximately equal amount for each tree-cover level into account
-method = 'boost' # 'svr' # 
+use_lgbm = True # faster than sklearn
 target= 'tree_cover' # 'land_use_category' # 
+
+
+do_scale_X = False # use a MinMaxScaler to bring the data into a range bewteen -1 and 1 -> no need.
+do_weight = False # assign a weight to each feature s.t. those occuring less frequent will have higher weights
+method = 'boost' # 'svr' # 
 kernel = 'rbf' # for svr
 
+# params for lgb:
+objective= 'huber' # 'mean_absolute_error'
 
-bastin_cols = ['longitude','latitude','dryland_assessment_region','Aridity_zone','land_use_category','tree_cover']
+
+# cols to be dropped from the training data. Aridity Zone can be kept.
+bastin_cols = ['longitude','latitude','dryland_assessment_region','land_use_category','tree_cover'] # 'Aridity_zone'
 
 
 def load_data(path, cols=None):
@@ -49,8 +60,15 @@ def load_data(path, cols=None):
     df.drop_duplicates(inplace=True)
     df.reset_index(inplace=True)
     t, X = df.tree_cover, df[cols]
-    cat = X.columns[X.dtypes == "object"]
-    X = pd.get_dummies(X,cat,drop_first=True)
+    
+    if use_lgbm:
+        if 'Aridity_zone' in X.columns:
+            X.Aridity_zone = pd.Categorical(X.Aridity_zone)
+        if 'dryland_assessment_region' in X.columns:
+            X.dryland_assessment_region = pd.Categorical(X.dryland_assessment_region)
+    else:
+        X = prep(X)
+    
     cover_to_count = df.groupby('tree_cover').count().iloc[:,0].to_dict()
     # t, X = sk.utils.shuffle(df.tree_cover, df[cols], random_state=1)
     # split = int(X.shape[0] * 0.9)
@@ -63,6 +81,7 @@ def prep(X):
     cat = X.columns[X.dtypes == "object"]
     X = pd.get_dummies(X,cat,drop_first=True)
     return X
+
 
 @timer
 def train(X, t, gridsearch=False, weights=None):
@@ -85,32 +104,56 @@ def train(X, t, gridsearch=False, weights=None):
         # warn if present!!!
     
     if gridsearch == True:
-        params = {
-            "n_estimators": [550],
-            "max_depth": [10,12],
-            "learning_rate": [0.01,0.03],
-            "loss": ["ls"],
-        }   # Hyperparameter tuning
-        clf = en.GradientBoostingRegressor()
-        cv = GridSearchCV(clf,params,cv=3, 
-                          n_jobs=4, verbose=1)
+        if use_lgbm:
+            depth = 12
+            # score: 0.38406944683157623
+            # best for depth 8: {'min_data_in_leaf': 50, 'n_estimators': 1000, 'num_leaves': 64}
+            # run with mae: {'min_data_in_leaf': 60, 'n_estimators': 2000, 'num_leaves': 32}
+            param_grid = {
+                'num_leaves': [2**(depth-2)], #[2**(depth-3), 2**(depth-2), 2**(depth-1)],
+                'min_child_samples': [50], # [40, 50, 60],
+                'n_estimators': [2000, 3000, 4000]
+            }
+            clf = lgb.LGBMRegressor(objective=objective, boosting_type='dart', alpha=0.9, learning_rate=0.1, max_depth=depth, random_state=42, subsample=1.0)
+            cv = GridSearchCV(clf, param_grid, n_jobs=2, cv=3, verbose=1, scoring='neg_mean_absolute_error')
+        else:
+            params = {
+                "n_estimators": [550],
+                "max_depth": [10,12],
+                "learning_rate": [0.01,0.03],
+                "loss": ["ls"],
+            }   # Hyperparameter tuning
+            clf = en.GradientBoostingRegressor()
+            cv = GridSearchCV(clf,params,cv=3, 
+                              n_jobs=4, verbose=1)
         cv.fit(X,t)
-        #os.rename('model.joblib', 'model.joblib.bk')
         jl.dump(cv, model_name)
         return cv 
     else:
         if target=='land_use_category':
-            clf = en.GradientBoostingClassifier(loss='deviance', criterion='friedman_mse', init=None,
-                              learning_rate=0.01, max_depth=8,  n_estimators=550,
-                              max_features=None, max_leaf_nodes=None,
-                              min_impurity_decrease=0.0, min_impurity_split=None,
-                              min_samples_leaf=1, min_samples_split=2,
-                              min_weight_fraction_leaf=0.0,
-                              n_iter_no_change=None, presort='auto',
-                              random_state=None, subsample=1.0, tol=0.0001,
-                              validation_fraction=0.1, verbose=0, warm_start=False)
+            kwargs = dict(loss='deviance', criterion='friedman_mse', init=None,
+                          learning_rate=0.01, max_depth=8,  n_estimators=550,
+                          max_features=None, max_leaf_nodes=None,
+                          min_impurity_decrease=0.0, min_impurity_split=None,
+                          min_samples_leaf=1, min_samples_split=2,
+                          min_weight_fraction_leaf=0.0,
+                          n_iter_no_change=None, presort='auto',
+                          random_state=None, subsample=1.0, tol=0.0001,
+                          validation_fraction=0.1, verbose=0, warm_start=False)
+            clf = en.GradientBoostingClassifier(**kwargs) if not use_lgbm else lgb.LGBMClassifier(**kwargs)
         else:
-            clf = en.GradientBoostingRegressor(alpha=0.9, criterion='friedman_mse', init=None,
+            if use_lgbm:
+                depth = 8
+                kwargs = dict(
+                        objective=objective, # regression, regression_l1, ...
+                        boosting_type='dart',
+                        alpha=0.9,
+                        learning_rate=0.1, max_depth=depth, num_leaves=2**(depth-2),
+                        min_child_samples=50,
+                        n_estimators=5000,
+                        random_state=42, subsample=1.0)
+            else:
+                kwargs = dict(alpha=0.9, criterion='friedman_mse', init=None,
                               learning_rate=0.1, loss='huber', max_depth=5, # rate 0.01, depth 8
                               max_features=None, max_leaf_nodes=None,
                               min_impurity_decrease=0.0, min_impurity_split=None,
@@ -120,10 +163,14 @@ def train(X, t, gridsearch=False, weights=None):
                               random_state=None, subsample=1.0, tol=0.0001,
                               validation_fraction=0.1, verbose=0, warm_start=False)
 
+            clf = en.GradientBoostingRegressor(**kwargs) if not use_lgbm else lgb.LGBMRegressor(**kwargs)
+
         clf.fit(X, t, sample_weight=weights)
         #os.rename('model.joblib', 'model.joblib.bk')
         jl.dump(clf, model_name)
         return clf
+
+
 
 @timer
 def train_svr(X, y, weights=None):
@@ -205,55 +252,28 @@ def evaluate(p, y_train_pred, y_test, y_train, w_test, w_train, second_run=False
     median = sorted(np.sqrt(diff**2))[int(len(diff)/2)]
     mean = sum(np.sqrt(diff**2))/len(diff)
     for i in range(0,100, 10):
-        print(str(i)+"% percentile : " +str(sorted(round(np.sqrt(diff**2)), 3)[int((len(diff))*i/100)]))
+        print(str(i)+"% percentile : " +str(round(sorted(np.sqrt(diff**2))[int((len(diff))*i/100)], 3)))
     print(f'Median error: {median:4f}, Mean error: {mean:4f}')
     plt.plot(sorted(np.sqrt(y_t_bt)**2))
     plt.plot(sorted(np.sqrt(diff**2)))
     plt.show()
     
     # how is it performing on the different tree cover estimates?
+    print('MAE on the different buckets:')
     for i in range(0,10):
         denom = len(diff[np.logical_and((i/10 + 0.1) > y_t_bt, (i/10) < y_t_bt)])
         if denom == 0:
             continue
-        nom = sum(np.sqrt(diff[np.logical_and((i/10 + 0.1) > y_t_bt, (i/10) < y_t_bt)]**2))
-        print(str(i)+' '+str(round(nom/denom, 3)*100))
-
+        nom = sum(np.abs(diff[np.logical_and((i/10 + 0.1) > y_t_bt, (i/10) < y_t_bt)]))
+        print(f'{i}: {round(nom/denom,3)}')
+        
 
 def main():
     os.chdir(w_dir)
     feat = ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B10", "B11", "sr_aerosol", "pixel_qa",
             "radsat_qa", "B1_sd", "B2_sd", "B3_sd", "B4_sd", "B5_sd", "B6_sd", "B7_sd", "B10_sd",
-            "B11_sd", "sr_aerosol_sd", "pixel_qa_sd", "radsat_qa_sd",
-            "dryland_assessment_region", "Aridity_zone"]
-    #feat = ['dryland_assessment_region', 'Aridity_zone',
-    #   'min(B1_m)', 'max(B1_m)',
-    #   'avg(B1_m)', 'stdev(B1_m)', 'median(B1_m)', 'lower_quartile(B1_m)',
-    #   'upper_quartile(B1_m)', 'min(B2_m)', 'max(B2_m)', 'avg(B2_m)',
-    #   'stdev(B2_m)', 'median(B2_m)', 'lower_quartile(B2_m)',
-    #   'upper_quartile(B2_m)', 'min(B3_m)', 'max(B3_m)', 'avg(B3_m)',
-    #   'stdev(B3_m)', 'median(B3_m)', 'lower_quartile(B3_m)',
-    #   'upper_quartile(B3_m)', 'min(B4_m)', 'max(B4_m)', 'avg(B4_m)',
-    #   'stdev(B4_m)', 'median(B4_m)', 'lower_quartile(B4_m)',
-    #   'upper_quartile(B4_m)', 'min(B5_m)', 'max(B5_m)', 'avg(B5_m)',
-    #   'stdev(B5_m)', 'median(B5_m)', 'lower_quartile(B5_m)',
-    #   'upper_quartile(B5_m)', 'min(B6_m)', 'max(B6_m)', 'avg(B6_m)',
-    #   'stdev(B6_m)', 'median(B6_m)', 'lower_quartile(B6_m)',
-    #    'upper_quartile(B6_m)', 'min(B7_m)', 'max(B7_m)', 'avg(B7_m)',
-    #    'stdev(B7_m)', 'median(B7_m)', 'lower_quartile(B7_m)',
-    #    'upper_quartile(B7_m)', 'min(B8_m)', 'max(B8_m)', 'avg(B8_m)',
-    #    'stdev(B8_m)', 'median(B8_m)', 'lower_quartile(B8_m)',
-    #    'upper_quartile(B8_m)', 'min(B8A_m)', 'max(B8A_m)', 'avg(B8A_m)',
-    #    'stdev(B8A_m)', 'median(B8A_m)', 'lower_quartile(B8A_m)',
-    #    'upper_quartile(B8A_m)', 'min(B9_m)', 'max(B9_m)', 'avg(B9_m)',
-    #    'stdev(B9_m)', 'median(B9_m)', 'lower_quartile(B9_m)',
-    #    'upper_quartile(B9_m)', 'min(B11_m)', 'max(B11_m)', 'avg(B11_m)',
-    #    'stdev(B11_m)', 'median(B11_m)', 'lower_quartile(B11_m)',
-    #    'upper_quartile(B11_m)', 'min(B12_m)', 'max(B12_m)', 'avg(B12_m)',
-    #    'stdev(B12_m)', 'median(B12_m)', 'lower_quartile(B12_m)',
-    #    'upper_quartile(B12_m)', 'min(NDVI_m)', 'max(NDVI_m)', 'avg(NDVI_m)',
-    #    'stdev(NDVI_m)', 'median(NDVI_m)', 'lower_quartile(NDVI_m)',
-    #    'upper_quartile(NDVI_m)', 'veg_pc']
+            "B11_sd", "sr_aerosol_sd", "Aridity_zone"] # "pixel_qa_sd", "radsat_qa_sd", "dryland_assessment_region", 
+
     if path.endswith('.parquet'):
         t, X, cnt_dict, feat = load_data(path=path)
     else:
@@ -267,8 +287,6 @@ def main():
         t[t==1] = 0.9999
         t = np.log(t/(1-t))
         
-    X = prep(X)
-    
     if do_scale_X:
         mm_scaler = MinMaxScaler(feature_range=(-1,1))
         X.iloc[:,:] = mm_scaler.fit_transform(X)
@@ -282,7 +300,7 @@ def main():
 
     if do_train:
         if method == 'boost':
-            model = train(X_train, y_train, weights=w_train)
+            model = train(X_train, y_train, weights=w_train, gridsearch=False)
         elif method == 'svr':
             model = train_svr(X_train, y_train, weights=w_train)
         p, model = predict(X_test, model=model)
@@ -326,7 +344,10 @@ def main():
         feat_df.sort_values('importance', ascending=False, inplace=True)
         feat_df.reset_index(inplace=True)
         print(feat_df[['feature', 'importance']].to_string())
-    
+        
+        n=25
+        feat_df[['feature', 'importance']][:n].plot.bar(x='feature', y='importance', legend=None, title='Feature Importance')
+        
 
 if __name__ == "__main__":
     main()
